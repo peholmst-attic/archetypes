c25cc984795227b1a3ed83fc8a7c9e39c41d6106
Petter Holmstrom <petter.holmstrom@abo.fi>
2013-08-27 15:40:20 +0300

    Added archetype for single module Vaadin project

---
+++ single-module-vaadin/src/main/resources/archetype-resources/webapp/src/main/java/__appName__UI.java
@@ -0,0 +1,29 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+import javax.servlet.annotation.WebServlet;
+
+@Widgetset("${package}.widgetset.${appName}Widgetset")
+@Theme("${appName}")
+public class ${appName}UI extends UI {
+
+    @WebServlet(urlPatterns = "/*")
+    @VaadinServletConfiguration(ui = ${appName}UI.class, productionMode = false)
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        setContent(new Label("Hello World"));
+    }
+}
